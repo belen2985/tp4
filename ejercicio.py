873fc33c43dfc2f7c71015693a64ac4fbeec21af
belen2985 <bellgimenez.bg@gmail.com>
2020-10-02 20:27:17 -0300

    PRIMER COMMIT CONSIGNA

---
+++ ejercicio.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+"""
+Consigna:Crea una lista vacía (pongamos 10 posiciones), pide sus valores
+y devuelve la suma, el promedio y la desviacion estandar de los números.
+Imprime la lista de numeros.
+"""
