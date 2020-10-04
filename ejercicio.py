7023dfc9604a43b05c3feae297d91d62727ffb05
belen2985 <bellgimenez.bg@gmail.com>
2020-10-04 13:47:55 -0300

    segundo commit, primera parte del programa

--- ejercicio.py
+++ ejercicio.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
 """
-Consigna:Crea una lista vacía (pongamos 10 posiciones), pide sus valores
-y devuelve la suma, el promedio y la desviacion estandar de los números.
+Consigna:Crea una lista vacÃ­a (pongamos 10 posiciones), pide sus valores
+y devuelve la suma, el promedio y la desviacion estandar de los nÃºmeros.
 Imprime la lista de numeros.
 """
+lista=[]
+
+suma=0
+prom=0
+num=0
+for i in range(10):
+    num = int(input("Ingrese un número: "))
+    lista.append(num)
+    suma += num
