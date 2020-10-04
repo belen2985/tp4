ca81774f577f4840305c2378f93db11d9010bf95
belen2985 <bellgimenez.bg@gmail.com>
2020-10-04 17:35:06 -0300

    finalización del ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
 """
-Consigna:Crea una lista vacÃ­a (pongamos 10 posiciones), pide sus valores
-y devuelve la suma, el promedio y la desviacion estandar de los nÃºmeros.
+Consigna:Crea una lista vacÃƒÂ­a (pongamos 10 posiciones), pide sus valores
+y devuelve la suma, el promedio y la desviacion estandar de los nÃƒÂºmeros.
 Imprime la lista de numeros.
 """
 lista=[]
-
 suma=0
 prom=0
 num=0
 for i in range(10):
-    num = int(input("Ingrese un número: "))
+    num = int(input("Ingrese un nÃºmero: "))
     lista.append(num)
     suma += num
 for i in lista:
     print(i)
 
-media = suma / len(lista)
+prom = suma / len(lista)
 
 print("La suma es ",suma)
-print("El promedio es ",promedio)
+print("El promedio es ",prom)
